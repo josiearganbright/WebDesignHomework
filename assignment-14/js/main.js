b241b78ba63cef87a85cb3f3a3d74879ad79eef3
josiearganbright <ja179616@umconnect.umt.edu>
2024-05-10 20:42:18 -0600

    Update main.js

--- assignment-14/js/main.js
+++ assignment-14/js/main.js
@@ -1,2 +1,48 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+
+ctx.moveTo(0,150);
+ctx.lineTo(500,150);
+ctx.stroke();
+
+ctx.moveTo(150,0);
+ctx.lineTo(150,500);
+ctx.stroke();
+
+ctx.moveTo(400,150);
+ctx.lineTo(400,500);
+ctx.stroke();
+
+ctx.moveTo(400,425);
+ctx.lineTo(150,425);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(400, 150);
+ctx.lineTo(400, 250);
+ctx.lineTo(500, 250);
+ctx.lineTo(500,150);
+ctx.closePath();
+ctx.fillStyle = 'blue';
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(0, 150);
+ctx.lineTo(150, 150);
+ctx.lineTo(150, 0);
+ctx.lineTo(0,0);
+ctx.closePath();
+ctx.fillStyle = 'red';
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(150, 500);
+ctx.lineTo(150, 425);
+ctx.lineTo(300, 425);
+ctx.lineTo(300,500);
+ctx.closePath();
+ctx.fillStyle = 'yellow';
+ctx.fill();
+ctx.stroke();
